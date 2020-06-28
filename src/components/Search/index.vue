2cb093a14d257d2a7f544aa75e69e5135eb3cba7
ah_kevin_xy <ah_kevin_xy@126.com>
2020-06-28 11:28:17 +0800

    first components

---
+++ src/components/Search/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div @click="getData">{{name}}</div>
+        <slot name="title" v-bind:user="user">
+
+                    这是一个测试的cacao
+            
+        </slot>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name:"iSerach",
+    props:{
+        name: String,
+        List:[],
+        user:Object
+    },
+    data(){
+        return {
+            userInfo:{
+                name:'xx',
+                age:'18'
+            }
+        }
+    },
+    methods:{
+        getData(){
+            console.log('xxxzz',111)
+            this.$emit('s',this.userInfo)
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/opencodes/alert/src/main.vue
@@ -0,0 +1,54 @@
+<template>
+    <transition>
+        <div
+            class="op-alert"
+            :class="[typeClass,center ?'is-center':'','is-'+effect]"
+            v-show="visible"
+            role="alert"
+        >
+            <div class="op-alert__content">
+                <span class="op-alert_title">
+                    <slot name="title">{{ title }}</slot>
+                </span>
+                <p class="op-alert__description" v-if="$slots.default && !description"> <slot></slot></p>
+                <p class="op-alert__description" v-if="$slots.default && !description"> {{ description }}</p>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+    export default {
+        name: "OcAlert",
+        props: {
+            title: {
+                type: String,
+                default: '这是一个测试弹框'
+            },
+            description: {
+                type: String,
+                default: '这是一个简单的描述'
+            },
+            type: {
+                type: Boolean,
+                default: true
+            },
+            closable: {
+                type: Boolean,
+                default: true
+            },
+            showIcon: {
+                type: Boolean,
+                default: true
+            },
+            closeText: {
+                type: String,
+                default: ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
