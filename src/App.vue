17f4fa5411e1a9161a6d7b1f9b320fc8e6e7cbbc
ah_kevin_xy <ah_kevin_xy@126.com>
2020-06-29 16:07:08 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
         <div>
-            test
+
+            <i class="el-icon-platform-eleme" style="color: red;font-size: 20px"></i>
+
+
+            <el-button icon="el-icon-delete" type="danger" circle autofocus></el-button>
         </div>
 </template>
 
@@ -10,6 +14,31 @@
     }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+    .el-row {
+        margin-bottom: 20px;
+    &:last-child {
+         margin-bottom: 0;
+     }
+    }
+    .el-col {
+        border-radius: 4px;
+    }
+    .bg-purple-dark {
+        background: #99a9bf;
+    }
+    .bg-purple {
+        background: #d3dce6;
+    }
+    .bg-purple-light {
+        background: #e5e9f2;
+    }
+    .grid-content {
+        border-radius: 4px;
+        min-height: 36px;
+    }
+    .row-bg {
+        padding: 10px 0;
+        background-color: #f9fafc;
+    }
 </style>
